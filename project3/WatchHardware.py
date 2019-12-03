f56b199907501728d0c371deeba7e77520607774
Jarkko Kovala <jarkko.kovala@iki.fi>
2019-12-03 17:50:37 +0200

    small changes

--- project3/WatchHardware.py
+++ project3/WatchHardware.py
@@ -2,15 +2,15 @@ import time
 import ThreadUtil
 
 class WatchHardware:
-    def __init__(self, eventhandler, gui):
+    def __init__(self, eventhandler):
         self.eventhandler = eventhandler
-        self.gui = gui
         self.timeTicks = 0
     
     def ThreadTimeTick(self):
         while True:
             time.sleep(1)
             self.timeTicks += 1
+            self.eventhandler.event("Time Advance")
 
     def start(self):
         ThreadUtil.StartThread(self.ThreadTimeTick, ())
