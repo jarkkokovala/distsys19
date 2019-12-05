a1e50421fd2e45babf4b4f2d5eca0e85c84af5a1
Jarkko Kovala <jarkko.kovala@iki.fi>
2019-12-05 09:42:12 +0200

    add time delay before shutting backlight

--- project3/WatchHardware.py
+++ project3/WatchHardware.py
@@ -5,12 +5,27 @@ class WatchHardware:
     def __init__(self, eventhandler):
         self.eventhandler = eventhandler
         self.timeTicks = 0
-    
+        self.timeUpdating = True
+        self.topRightPressed = False
+
+    def topRightDown(self):
+        self.topRightPressed = True
+
+    def ThreadShutLight(self):
+        time.sleep(2)
+        if not self.topRightPressed:
+            self.eventhandler.event("shutLight")
+
+    def topRightUp(self):
+        self.topRightPressed = False
+        ThreadUtil.StartThread(self.ThreadShutLight, ())
+
     def ThreadTimeTick(self):
         while True:
             time.sleep(1)
-            self.timeTicks += 1
-            self.eventhandler.event("increaseTimeByOne")
+            if self.timeUpdating:
+                self.timeTicks += 1
+                self.eventhandler.event("increaseTimeByOne")
 
     def start(self):
         ThreadUtil.StartThread(self.ThreadTimeTick, ())
